ed7bccb9b02e35439ceaa6b2ecd1863aadb8b96b
Julio Matheus Vaz <jmatheus.vaz@gmail.com>
2016-10-03 16:05:11 -0300

    heroku push

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
   def index
     @mainTitle = "Welcome in Inspinia Rails Seed Project"
     @mainDesc = "It is an application skeleton for a typical Ruby on Rails web app. You can use it to quickly bootstrap your webapp projects and dev/prod environment."
-    @building_locations = BuildingLocation.all
+    reservation_hash = Reservation.joins(:building_location).all.group(:name).count.to_h
+    @reservation_values = reservation_hash.values
+    @reservation_keys = reservation_hash.keys
   end
 
   def minor
