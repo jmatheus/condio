4255583e22cb4ffe1d70cf387e2dd11096d5a416
Julio Matheus Vaz <jmatheus.vaz@gmail.com>
2016-10-03 11:46:07 -0300

    heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Condio::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get "home/index"
   get "home/minor"
-  get 'products_json', :defaults => { :format => 'json' }
+  get 'products_json', to: 'products#index_json', :defaults => { :format => 'json' }
+  get 'building_messages_json', to: 'building_messages#index_json', defaults: { format: 'json'}
   resources :products
-  get 'building_messages_json', :defaults => { :format => 'json' }
   resources :building_messages
-  get 'building_locations/dates/:id', to: 'building_locations#dates', as: 'building_locations_dates'
-  resources :building_locations
-  get 'reservations/history'
-  resources :reservations
-  get 'users/:id/bills', to: 'bills#user_bills', as: 'user_bills'
-  resources :users
-  resources :bills
-  resources :devices
+  get 'building_locations/dates/:id', to: 'building_locations#dates', as: 'building_locations_dates', defaults: { format: 'json'}
+  resources :building_locations, defaults: { format: 'json'}
+  get 'reservations/history', defaults: { format: 'json'}
+  resources :reservations, defaults: { format: 'json'}
+  get 'users/:id/bills', to: 'bills#user_bills', as: 'user_bills', defaults: { format: 'json'}
+  resources :users, defaults: { format: 'json'}
+  resources :bills, defaults: { format: 'json'}
+  resources :devices, defaults: { format: 'json'}
   root to: 'home#index'
-  end
+end
