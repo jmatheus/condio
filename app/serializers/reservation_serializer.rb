e49d85b53a09da423d5a695a51928b73bb9381e5
Julio Matheus Vaz <jmatheus.vaz@gmail.com>
2016-10-03 01:23:07 -0300

    heroku push

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,7 +1,7 @@
 class ReservationSerializer < ActiveModel::Serializer
   attributes :id, :event_date, :guest_quantity, :start_hour, :end_hour, :reservation_name
   # has_one :user
-  # has_one :building_location
+  has_one :building_location
 
   def event_date
     object.event_date.try(:strftime, "%d %b")
